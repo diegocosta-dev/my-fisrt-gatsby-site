c82d65ddb7b16eaf118ef525d13ac9f892a42a27
Diego Arthur Costa <troiablender@gmail.com>
2022-06-10 17:15:14 -0300

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.jsx
+++ src/pages/blog/{mdx.slug}.jsx
@@ -1,26 +1,45 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { getImage, GatsbyImage} from 'gatsby-plugin-image'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../../layouts/basicLayout'
 
 const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <small>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </small>
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
     </Layout>
   )
 }
+
 export const query = graphql`
-  query ($id: String) {
+  query($id: String) {
     mdx(id: {eq: $id}) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM, D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `
